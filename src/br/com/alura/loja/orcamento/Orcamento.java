2c0a620a63735e10723b7a51cac7d24247c68991
Caitano <josiasmartinscaitano@gmail.com>
2023-06-01 20:37:02 -0300

    feat: pattern State

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -2,15 +2,37 @@ package br.com.alura.loja.orcamento;
 
 import java.math.BigDecimal;
 
+import br.com.alura.loja.orcamento.situacao.EmAnalise;
+import br.com.alura.loja.orcamento.situacao.SituacaoOrcamento;
+
 public class Orcamento {
 
 	
 	private BigDecimal valor;
 	private int quantidadeItens;
+	private SituacaoOrcamento situacao;
 	
 	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		this.valor = valor;
 		this.quantidadeItens = quantidadeItens;
+		this.situacao = new EmAnalise();
+	}
+	
+	public void aprovar() {
+		this.situacao.aprovar(this);
+	}
+	
+	public void reprovar() {
+		this.situacao.reprovar(this);
+	}
+	
+	public void finalizar() {
+		this.situacao.finalizar(this);
+	}
+	
+	public void aplicarDescontoExtra() {
+		BigDecimal valorDoDescontoExtra = this.situacao.calcularValorDescontoExtra(this);
+		this.valor = this.valor.subtract(valorDoDescontoExtra);
 	}
 	
 	public BigDecimal getValor() {
@@ -19,6 +41,14 @@ public class Orcamento {
 	
 	public int getQuantidadeItens() {
 		return this.quantidadeItens;
+	}
+	
+	public SituacaoOrcamento getSituacao() {
+		return this.situacao;
+	}
+	
+	public void setSituacao(SituacaoOrcamento situacao) {
+		this.situacao = situacao;
 	}
 	
 }
---
+++ src/br/com/alura/loja/orcamento/situacao/Finalizado.java
@@ -0,0 +1,21 @@
+package br.com.alura.loja.orcamento.situacao;
+
+import java.math.BigDecimal;
+
+import br.com.alura.loja.orcamento.Orcamento;
+
+public class Finalizado extends SituacaoOrcamento {
+	
+//	public BigDecimal calcularValorDescontoExtra(Orcamento orcamento) {
+//		
+//	}
+//	
+//	public void aprovar(Orcamento orcamento) {
+//		orcamento.setSituacao(new Finalizar());
+//	}
+//	
+//	public void reprovar(Orcamento orcamento) {
+//		orcamento.setSituacao(new Reprovado());
+//	}
+
+}
