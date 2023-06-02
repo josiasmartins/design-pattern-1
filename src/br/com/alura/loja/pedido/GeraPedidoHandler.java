92af5be74a955c05e63690fc543f2a5fe91fac92
Caitano <josiasmartinscaitano@gmail.com>
2023-06-02 08:58:06 -0300

    feat: observer

--- src/br/com/alura/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/alura/loja/pedido/GeraPedidoHandler.java
@@ -1,20 +1,30 @@
 package br.com.alura.loja.pedido;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import br.com.alura.loja.orcamento.Orcamento;
+import br.com.alura.loja.pedido.acao.AcaoAposGerarPedido;
+import br.com.alura.loja.pedido.acao.EnviarEmailPedido;
+import br.com.alura.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 public class GeraPedidoHandler {
 	
 	// constrcutor com injecao de depedencias: repository, service, etc;
+	
+	private List<AcaoAposGerarPedido> acoes;
+	
+	public GeraPedidoHandler(List<AcaoAposGerarPedido> acoes) {
+		this.acoes = acoes;
+	}
 	
 	public void executa(GeraPedido dados) {
 		Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQuantidadeItens());
 		
 		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
 		
-		System.out.println("Salvar pedido no Banco de Dados");
-		System.out.println("Enviar email com dados do novo pedido");
+		acoes.forEach(a -> a.executarAcao(pedido));
+		
 	}
 
 }
